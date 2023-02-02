c15ad6a0473da23029f1ad2f6431c5b997d7a3ff
Yakup Atik <104201667+SwachTR@users.noreply.github.com>
2023-02-02 12:12:27 +0300

    Create SelectionSort.cs

---
+++ SelectionSort.cs
@@ -0,0 +1,30 @@
+Proje 1
+[22, 27, 16, 2, 18, 6] -> Insertion Sort
+
+1. Yukarı verilen dizinin sort türüne göre aşamalarını yazınız.
+
+1. aşama => [2, 27, 16, 22, 18, 6]		
+2. aşama => [2, 6, 16, 22, 18, 27]		
+3. aşama => [2, 6, 16, 18, 22, 27]		
+
+2. Big-O gösterimini yazınız.
+
+2. pBig-O :	O(n^2)
+  π
+3. Time Complexity: Dizi sıralandıktan sonra 18 sayısı aşağıdaki case'lerden hangisinin kapsamına girer? Yazınız
+
+Average case: Aradığımız sayının ortada olması
+Worst case: Aradığımız sayının sonda olması
+Best case: Aradığımız sayının dizinin en başında olması.
+
+3. 18 average case kapsamına girer.
+
+4. [7,3,5,8,2,9,4,15,6] dizisinin Selection Sort'a göre ilk 4 adımını yazınız.
+
+1. aşama => [2, 3, 5, 8, 7, 9, 4, 15, 6]
+
+2. aşama => [2, 3, 4, 8, 7, 9, 5, 15, 6]
+
+3. aşama => [2, 3, 4, 5, 7, 9, 8, 15, 6]
+
+4. aşama => [2, 3, 4, 5, 6, 9, 8, 15, 7]
